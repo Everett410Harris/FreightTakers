cda731192091d8a2859733da7c7988540462212a
Everett Harris <eve1harris2638@outlook.com>
2023-03-21 04:29:27 -0400

    renamed form, added lbls and pics

---
+++ FrieghtTakers/Drivers.Designer.cs
@@ -0,0 +1,304 @@
+﻿namespace FrieghtTakers
+{
+    partial class Drivers
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Drivers));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.LogoPicBx = new System.Windows.Forms.PictureBox();
+            this.LogoPnl = new System.Windows.Forms.Panel();
+            this.DriversPnl = new System.Windows.Forms.Panel();
+            this.DriversLblPicBx = new System.Windows.Forms.Label();
+            this.DriversPicBx = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.label2 = new System.Windows.Forms.Label();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.label3 = new System.Windows.Forms.Label();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.label4 = new System.Windows.Forms.Label();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.panel5 = new System.Windows.Forms.Panel();
+            ((System.ComponentModel.ISupportInitialize)(this.LogoPicBx)).BeginInit();
+            this.LogoPnl.SuspendLayout();
+            this.DriversPnl.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.DriversPicBx)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.panel3.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            this.panel4.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            this.panel5.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.White;
+            this.panel1.Location = new System.Drawing.Point(106, 2);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1146, 917);
+            this.panel1.TabIndex = 0;
+            // 
+            // LogoPicBx
+            // 
+            this.LogoPicBx.Image = ((System.Drawing.Image)(resources.GetObject("LogoPicBx.Image")));
+            this.LogoPicBx.Location = new System.Drawing.Point(3, 3);
+            this.LogoPicBx.Name = "LogoPicBx";
+            this.LogoPicBx.Size = new System.Drawing.Size(46, 44);
+            this.LogoPicBx.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.LogoPicBx.TabIndex = 1;
+            this.LogoPicBx.TabStop = false;
+            // 
+            // LogoPnl
+            // 
+            this.LogoPnl.Controls.Add(this.LogoPicBx);
+            this.LogoPnl.Location = new System.Drawing.Point(1, 2);
+            this.LogoPnl.Name = "LogoPnl";
+            this.LogoPnl.Size = new System.Drawing.Size(55, 49);
+            this.LogoPnl.TabIndex = 2;
+            // 
+            // DriversPnl
+            // 
+            this.DriversPnl.BackColor = System.Drawing.Color.White;
+            this.DriversPnl.Controls.Add(this.DriversLblPicBx);
+            this.DriversPnl.Controls.Add(this.DriversPicBx);
+            this.DriversPnl.ForeColor = System.Drawing.Color.White;
+            this.DriversPnl.Location = new System.Drawing.Point(21, 94);
+            this.DriversPnl.Name = "DriversPnl";
+            this.DriversPnl.Size = new System.Drawing.Size(79, 84);
+            this.DriversPnl.TabIndex = 0;
+            // 
+            // DriversLblPicBx
+            // 
+            this.DriversLblPicBx.AutoSize = true;
+            this.DriversLblPicBx.Font = new System.Drawing.Font("Georgia Pro", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.DriversLblPicBx.ForeColor = System.Drawing.Color.Lime;
+            this.DriversLblPicBx.Location = new System.Drawing.Point(7, 56);
+            this.DriversLblPicBx.Name = "DriversLblPicBx";
+            this.DriversLblPicBx.Size = new System.Drawing.Size(69, 18);
+            this.DriversLblPicBx.TabIndex = 1;
+            this.DriversLblPicBx.Text = "Drivers";
+            // 
+            // DriversPicBx
+            // 
+            this.DriversPicBx.Image = ((System.Drawing.Image)(resources.GetObject("DriversPicBx.Image")));
+            this.DriversPicBx.Location = new System.Drawing.Point(14, 3);
+            this.DriversPicBx.Name = "DriversPicBx";
+            this.DriversPicBx.Size = new System.Drawing.Size(51, 50);
+            this.DriversPicBx.TabIndex = 0;
+            this.DriversPicBx.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Georgia Pro", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Lime;
+            this.label1.Location = new System.Drawing.Point(7, 56);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(69, 18);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Drivers";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(14, 3);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(51, 50);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.White;
+            this.panel2.Controls.Add(this.label1);
+            this.panel2.Controls.Add(this.pictureBox1);
+            this.panel2.ForeColor = System.Drawing.Color.White;
+            this.panel2.Location = new System.Drawing.Point(21, 235);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(79, 84);
+            this.panel2.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Georgia Pro", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.Lime;
+            this.label2.Location = new System.Drawing.Point(7, 56);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(69, 18);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Drivers";
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+            this.pictureBox2.Location = new System.Drawing.Point(14, 3);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(51, 50);
+            this.pictureBox2.TabIndex = 0;
+            this.pictureBox2.TabStop = false;
+            // 
+            // panel3
+            // 
+            this.panel3.BackColor = System.Drawing.Color.White;
+            this.panel3.Controls.Add(this.label2);
+            this.panel3.Controls.Add(this.pictureBox2);
+            this.panel3.ForeColor = System.Drawing.Color.White;
+            this.panel3.Location = new System.Drawing.Point(21, 371);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(79, 84);
+            this.panel3.TabIndex = 2;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Georgia Pro", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.Lime;
+            this.label3.Location = new System.Drawing.Point(7, 56);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(69, 18);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "Drivers";
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox3.Image")));
+            this.pictureBox3.Location = new System.Drawing.Point(14, 3);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(51, 50);
+            this.pictureBox3.TabIndex = 0;
+            this.pictureBox3.TabStop = false;
+            // 
+            // panel4
+            // 
+            this.panel4.BackColor = System.Drawing.Color.White;
+            this.panel4.Controls.Add(this.label3);
+            this.panel4.Controls.Add(this.pictureBox3);
+            this.panel4.ForeColor = System.Drawing.Color.White;
+            this.panel4.Location = new System.Drawing.Point(21, 511);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(79, 84);
+            this.panel4.TabIndex = 2;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Georgia Pro", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.Lime;
+            this.label4.Location = new System.Drawing.Point(7, 56);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(69, 18);
+            this.label4.TabIndex = 1;
+            this.label4.Text = "Drivers";
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox4.Image")));
+            this.pictureBox4.Location = new System.Drawing.Point(14, 3);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(51, 50);
+            this.pictureBox4.TabIndex = 0;
+            this.pictureBox4.TabStop = false;
+            // 
+            // panel5
+            // 
+            this.panel5.BackColor = System.Drawing.Color.White;
+            this.panel5.Controls.Add(this.label4);
+            this.panel5.Controls.Add(this.pictureBox4);
+            this.panel5.ForeColor = System.Drawing.Color.White;
+            this.panel5.Location = new System.Drawing.Point(21, 653);
+            this.panel5.Name = "panel5";
+            this.panel5.Size = new System.Drawing.Size(79, 84);
+            this.panel5.TabIndex = 2;
+            // 
+            // Drivers
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1407, 931);
+            this.Controls.Add(this.panel5);
+            this.Controls.Add(this.panel4);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.DriversPnl);
+            this.Controls.Add(this.LogoPnl);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Drivers";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Drivers";
+            ((System.ComponentModel.ISupportInitialize)(this.LogoPicBx)).EndInit();
+            this.LogoPnl.ResumeLayout(false);
+            this.DriversPnl.ResumeLayout(false);
+            this.DriversPnl.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.DriversPicBx)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.panel3.ResumeLayout(false);
+            this.panel3.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            this.panel4.ResumeLayout(false);
+            this.panel4.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            this.panel5.ResumeLayout(false);
+            this.panel5.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.PictureBox LogoPicBx;
+        private System.Windows.Forms.Panel LogoPnl;
+        private System.Windows.Forms.Panel DriversPnl;
+        private System.Windows.Forms.Label DriversLblPicBx;
+        private System.Windows.Forms.PictureBox DriversPicBx;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.Panel panel4;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.Panel panel5;
+    }
+}
+
--- FrieghtTakers/Drivers.cs
+++ FrieghtTakers/Drivers.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace FrieghtTakers
 {
-    public partial class Form1 : Form
+    public partial class Drivers : Form
     {
-        public Form1()
+        public Drivers()
         {
             InitializeComponent();
         }
